766c8f0e07da4898d3c93a7783285fa8114fdc91
nishantsharan2603 <102582474+nishantsharan2603@users.noreply.github.com>
2024-09-30 20:52:56 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.85.0"
+      version = "4.0.0"
     }
   }
   backend "azurerm" {
     resource_group_name  = "avdstorageeasrg"
     storage_account_name = "avdeasstc02"
     container_name       = "tfstate"
-    key                  = "terraform1.tfstate"
+    key                  = "terraform-eas-sap-win11.tfstate"
   }
 
 }
