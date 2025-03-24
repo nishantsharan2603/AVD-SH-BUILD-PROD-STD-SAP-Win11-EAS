249cb89d519199b71c571aeb904088a6f20f02ea
nishantsharan2603 <102582474+nishantsharan2603@users.noreply.github.com>
2025-03-24 13:36:54 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,18 +2,27 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.0.0"
+      version = "4.18.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.2.0"
     }
   }
+
   backend "azurerm" {
     resource_group_name  = "avdstorageeasrg"
     storage_account_name = "avdeasstc02"
     container_name       = "tfstate"
     key                  = "terraform-eas-sap-win11.tfstate"
   }
-
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "azapi" {
+  enable_preflight = true
+}
+  
